647ca8365907dbbb8e9f62a957e0223d26c40cc4
Peroulis Constantinos <32259290+Peroulis@users.noreply.github.com>
2017-10-06 12:37:47 +0300

    Update DrawAxis.m

--- DrawAxis.m
+++ DrawAxis.m
@@ -1,7 +1,8 @@
 function DrawAxis(originX, originY, originZ,rot)
 % make a new quiver3 giving the individual origin
+% the configuration space of the links' start point throught time
 
-quiver3(originX,originY,originZ,rot(1,1),rot(1,2),rot(1,3))   % x vector of the tip
+quiver3(originX,originY,originZ,rot(1,1),rot(1,2),rot(1,3))   % x vector 
 hold on
 grid
 xlabel('x')
@@ -15,4 +16,3 @@ view([1 2 1])
 daspect([1 1 1])
 quiver3(originX,originY,originZ,rot(2,1),rot(2,2),rot(2,3))   % y vector
 quiver3(originX,originY,originZ,rot(3,1),rot(3,2),rot(3,3))  % z vector
-
